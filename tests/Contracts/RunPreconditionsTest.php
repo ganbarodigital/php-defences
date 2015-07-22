4538ecdc175121907c1b3174a83513d3a4eef65c
Stuart Herbert <stuart@stuartherbert.com>
2015-07-22 22:58:38 +0100

    New: aliases for RunPreconditions for better readability

---
+++ tests/Contracts/RunPreconditionsTest.php
@@ -0,0 +1,133 @@
+<?php
+
+/**
+ * Copyright (c) 2015-present Ganbaro Digital Ltd
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in
+ *     the documentation and/or other materials provided with the
+ *     distribution.
+ *
+ *   * Neither the names of the copyright holders nor the names of his
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @category  Libraries
+ * @package   Defensive/Contracts
+ * @author    Stuart Herbert <stuherbert@ganbarodigital.com>
+ * @copyright 2015-present Ganbaro Digital Ltd www.ganbarodigital.com
+ * @license   http://www.opensource.org/licenses/bsd-license.php  BSD License
+ * @link      http://code.ganbarodigital.com/php-defensive
+ */
+
+namespace GanbaroDigital\Defensive\Contracts;
+
+use PHPUnit_Framework_TestCase;
+
+/**
+ * @coversDefaultClass GanbaroDigital\Defensive\Contracts\RunPreconditions
+ */
+class RunPreconditionsTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @covers ::check
+     */
+    public function testExecutesCallbackIfWrappedContractsEnabled()
+    {
+        // ----------------------------------------------------------------
+        // setup your test
+
+        $executed = false;
+        $callback = function() use (&$executed) {
+            $executed = true;
+        };
+        WrappedContracts::enable();
+
+        // ----------------------------------------------------------------
+        // perform the change
+
+        RunPreconditions::check($callback, []);
+
+        // ----------------------------------------------------------------
+        // test the results
+
+        $this->assertTrue($executed);
+    }
+
+    /**
+     * @covers ::check
+     */
+    public function testDoesNotExecuteCallbackIfWrappedContractsDisabled()
+    {
+        // ----------------------------------------------------------------
+        // setup your test
+
+        $executed = false;
+        $callback = function() use (&$executed) {
+            $executed = true;
+        };
+        WrappedContracts::disable();
+
+        // ----------------------------------------------------------------
+        // perform the change
+
+        RunPreconditions::check($callback, []);
+
+        // ----------------------------------------------------------------
+        // test the results
+
+        $this->assertFalse($executed);
+    }
+
+    /**
+     * @covers ::check
+     */
+    public function testPassesParamsIntoCallback()
+    {
+        // ----------------------------------------------------------------
+        // setup your test
+
+        $expectedParam1 = 5;
+        $expectedParam2 = 3.1415927;
+
+        $executed = false;
+        $callback = function($param1, $param2) use (&$executed, $expectedParam1, $expectedParam2) {
+            $this->assertEquals($expectedParam1, $param1);
+            $this->assertEquals($expectedParam2, $param2);
+            $executed = true;
+        };
+        WrappedContracts::enable();
+
+        // ----------------------------------------------------------------
+        // perform the change
+
+        RunPreconditions::check($callback, [$expectedParam1, $expectedParam2]);
+
+        // ----------------------------------------------------------------
+        // test the results
+
+        $this->assertTrue($executed);
+    }
+
+}
